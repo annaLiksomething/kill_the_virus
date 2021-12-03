d31ed667100089db8dcca26f8da08e80f34f0882
Glitch (glitch-hello-website) <none>
2021-12-03 17:16:38 +0000

    🍅💪 Checkpoint ./sketch.js:39305712/187 ./.glitch-assets:39305712/364

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+var ghost;
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
+  ghost = createSprite(600, 200);
+  ghost.addAnimation('normal', 'assets/asterisk.png', 'assets/asterisk.png');
 }
 
 function draw() {
