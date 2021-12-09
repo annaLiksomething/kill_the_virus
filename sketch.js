21478eaf21a30678e15b8cd9f0fc27e5efd3b899
Glitch (glitch-hello-website) <none>
2021-12-09 22:07:28 +0000

    ❄️💃 Checkpoint ./sketch.js:39305712/996

--- sketch.js
+++ sketch.js
@@ -1,6 +1,32 @@
 var imageSprite;
 
 function setup() {
-  createCanvas(800, 300);
-  var img = loadImage('https://cdn.glitch.me/070b9542-c4af-439a-a169-8bdbf7f0427e%2Fasterisk.png?v=1638551533723');
+  createCanvas(300, 300);
 }
+
+function draw(){
+
+  background(0);
+
+  //fill(0);
+  textAlign(CENTER);
+  fill(255);
+  text('Click to create a new sprite', width/2, height/2);
+  //draw all the sprites added to the sketch so far
+  //the positions will be updated automatically at every cycle
+  drawSprites();
+}
+
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
+}
+
