c2d6891052f1f9aaaeba85453d03b9ef313fa93f
Glitch (glitch-hello-website) <none>
2021-12-09 21:55:23 +0000

    🏊🏼🍽 Checkpoint ./sketch.js:39305712/112

--- sketch.js
+++ sketch.js
@@ -2,5 +2,5 @@ var imageSprite;
 
 function setup() {
   createCanvas(800, 300);
-  var img = loadImage("assets/asterisk.png");
+  var img = loadImage('https://cdn.glitch.me/070b9542-c4af-439a-a169-8bdbf7f0427e%2Fasterisk.png?v=1638551533723');
 }
